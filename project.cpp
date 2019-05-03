90be6c2036f231f4fd2357a088ea0d7d367eedfd
gabrielleviray <gabrielle.viray@sjsu.edu>
2019-05-02 18:23:09 -0700

    cpp file

---
+++ project.cpp
@@ -0,0 +1,31 @@
+
+using namespace std;
+#include <string>
+
+
+
+class Car
+{
+    public:
+    Car()
+    Car(string licensePlate)
+    
+    string getLicensePlate() const;
+    ~Car();
+
+
+    private:
+    string licensePlate;
+    static int num_of_cars;
+};
+
+class ParkingLot{
+    private:
+        string name;
+
+};
+
+int main(){
+
+    return 0;
+}
