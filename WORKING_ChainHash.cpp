dcfd76ab7507543872c3f927e1fd83f542de5c30
csytnd <casey.tanod@sjsu.edu>
2019-05-05 11:28:00 -0700

    -created an array, 'c', for user to input 10 "cars" -we still need to assign the strings in the 'c' array to those in the 'a' array, where are random numbers lie.

--- WORKING_ChainHash.cpp
+++ WORKING_ChainHash.cpp
@@ -7,19 +7,33 @@ using namespace std;
 
 int main(){
 
-	const unsigned int length = 25;
+	string c[10];
+	const unsigned int length = 10;
 	srand(time(0));
+
+	cout << "Enter info for 10 cars: " << endl;
+	for (int i=0; i<length; i++) {
+		cout << i+1 << ")";
+		cin >> c[i];
+
+	}
+
+	cout << "Cars entered: ";
+	for (int i=0; i<length; i++) {
+		cout << c[i] << ", ";
+	}
+	cout << endl;
 
 	int a[length];
 		for (int i=0; i<length; i++) {
-			a[i] = rand()%100;
+			a[i] = rand()%50;
 			if (a[i] == a[i-1]) {
 				a[i] = a[i]+1;
 			}
 			cout << a[i] <<endl;
 		}
 
-	Hash h(30); // # = count of buckets
+	Hash h(length); // length = # of buckets
 
 	for (int i = 0; i < length; i++){
 		h.insertItem(a[i]);
